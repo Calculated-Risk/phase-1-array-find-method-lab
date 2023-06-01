709bf9e44bd2f237a91e7d56e97267007e994aeb
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-01 18:44:03 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(recordArray){
+    let winner = recordArray.find(record => record.result === 'W');
+    if (winner){
+        return winner.year 
+    }else{
+        return undefined;
+    }
+    
+}
+
+
+
+
+
+
